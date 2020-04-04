a9b1711e15662776537169fb1fd457a48621cbf9
Adrian Hodrea <adihodrea@yahoo.com>
2020-04-04 15:20:11 +0300

    task 1 completed - pluck

--- index.js
+++ index.js
@@ -9,4 +9,25 @@ const typeCastAndAdd = strArr => {
   return strArr.map( item => Number(item) + 2 );
 }
 
-console.log('Typecast: ', typeCastAndAdd(strArr));
+console.log('Task 0: ', typeCastAndAdd(strArr));
+
+/* ---------------------------------                ------------------------------------- */
+/* 
+1. Implement a function that receives an array of objects and a key name and returns an array with all the values corresponding to the key of the objects in the array.
+*/
+const demoArr = [
+  {id: 1, color: 'red', height: 15, width: 20, distance: 10},
+  {id: 2, color: 'green', height: 5, width: 30, distance: 15},
+  {id: 3, color: 'turqoize', height: 7, width: 9, distance: 8},
+  {id: 4, color: 'blue', height: 2, width: 3, distance: 3},
+  {id: 5, color: 'red', height: 10, width: 10, distance: 2},
+  {id: 6, color: 'crimson', height: 7, width: 8, distance: 16},
+];
+
+const pluck = (demoArr, keyName) => {
+  return demoArr.map( item => item[keyName]);
+};
+
+console.log('Task 1: ',pluck(demoArr, 'color'));  // => ['red', 'green', 'turqoize' .......];
+
+/* ---------------------------------                ------------------------------------- */
